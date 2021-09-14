6d34fae25d224a15e48dcdf6bff5a5da32e68f6d
bmuday <bmuday@live.fr>
2021-09-14 18:25:29 +0200

    new changes

--- test-package/script.js
+++
@@ -1,7 +0,0 @@
-const guessMyAge = require('guess-my-age')
-
-if (guessMyAge(process.argv[2])) {
-    console.log("Yeah!!! I'm a 27 fullstack JS developer from France. Nice to meet you buddy.")
-} else {
-    console.log("Bruh")
-}
